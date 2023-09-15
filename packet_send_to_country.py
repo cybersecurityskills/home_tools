6c06bc68b970e421e9b30e15dfcc7eda78777e6d
cybersecurityskills <139646528+cybersecurityskills@users.noreply.github.com>
2023-09-15 08:59:33 +0200

    Create packet_send_to_country.py

---
+++ packet_send_to_country.py
@@ -0,0 +1,25 @@
+from scapy.all import *
+import sys
+ 
+countries = {}
+countries["CHINA"] = '183.136.225.31'
+countries["CANADA"] = '159.203.46.152'
+countries["ITALY"] = '192.71.26.76'
+countries["VIETNAM"] = '123.30.149.76'
+countries["KOREA"] = '61.99.254.192'
+countries["MOLDOVA"] = '104.28.156.58'
+countries["SWEDEN"] = '185.246.130.69'
+countries["US"] = '45.134.144.113'
+countries["PAKISTAN"] = '101.53.233.81'
+ 
+if len(sys.argv) < 3:
+  print("Usage: packet_send_country.py COUNTRY SRC_IP DST_PORT\n")
+  print("Valid countries are:"," ".join(countries.keys()))
+  sys.exit()
+ 
+ 
+ip = IP(src=sys.argv[1],dst=countries[sys.argv[2]])/TCP(dport=int(sys.argv[3]),flags='S')
+p1 = (ip)
+print("Sending",p1,"TTL",p1.ttl)
+ 
+send(p1,iface="enp0s8")
