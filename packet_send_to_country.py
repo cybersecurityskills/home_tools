7ea04898ed1f1b7003ae239e6348524720724d04
cybersecurityskills <139646528+cybersecurityskills@users.noreply.github.com>
2023-09-15 09:41:22 +0200

    Update packet_send_to_country.py

--- packet_send_to_country.py
+++ packet_send_to_country.py
@@ -14,14 +14,14 @@ countries["US"] = '45.134.144.113'
 countries["PAKISTAN"] = '101.53.233.81'
  
 if len(sys.argv) < 3:
-  print("Usage: packet_send_to_country.py COUNTRY SRC_IP DST_PORT\n")
+  print("Usage: packet_send_country.py COUNTRY SRC_IP DST_PORT\n")
   print("Valid countries are:"," ".join(countries.keys()))
   sys.exit()
-
+ 
 sport=random.randint(1024,65535)
  
-ip = IP(src=sys.argv[2],dst=countries[sys.argv[1]])/TCP(sport=sport,dport=int(sys.argv[3]),flags='S')
+ip = Ether()/IP(src=sys.argv[2],dst=countries[sys.argv[1]])/TCP(sport=sport,dport=int(sys.argv[3]),flags='S')
 p1 = (ip)
 print("Sending",p1)
  
-send(p1,iface="enp0s8")
+sendp(p1,iface="enp0s8")
